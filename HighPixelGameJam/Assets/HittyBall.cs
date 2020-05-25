48ba84f81b6b8533e843562994174a761fa02c72
Alistair <AlistairJR302@Gmail.com>
2020-05-25 13:35:42 +0100

    HittyBall Script

--- HighPixelGameJam/Assets/HittyBall.cs
+++ HighPixelGameJam/Assets/HittyBall.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HittyBall : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Quaternion originalRotation;
+    public Text powerText;
+    int power = 0;
+
+    private void Start()
     {
-        
+        originalRotation = transform.rotation;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("a"))
+        {
+            transform.Rotate(0, 1, 0);
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            transform.Rotate(0, -1, 0);
+        }
+
+        if (Input.GetButton("Jump"))
+        {
+            power += 1;
+            Debug.Log("Pressed");
+        }else if (Input.GetButtonUp("Jump"))
+        {
+            HitBall();
+            Debug.Log("Unpressed");
+        }
+
+        powerText.text = "Power: " + power;
+    }
+
+    void HitBall()
+    {
+        transform.rotation = originalRotation;
+        transform.GetComponent<Rigidbody>().AddRelativeForce(-transform.forward * ((int)(power/100)),ForceMode.Impulse);
+        power = 0;
     }
 }
