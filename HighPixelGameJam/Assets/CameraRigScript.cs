e376a5adb861ff71139c6da381fcb1ea3d5b51a2
n-sweeney <nathan.james.sweeney@gmail.com>
2020-05-25 18:34:23 +0100

    Added Pan Cam

---
+++ HighPixelGameJam/Assets/CameraRigScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRigScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Ball;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.transform.position = Ball.transform.position;
+    }
+}
--- HighPixelGameJam/Assets/ThirdPersonCameraController.cs
+++ HighPixelGameJam/Assets/ThirdPersonCameraController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ThirdPersonCameraController : MonoBehaviour
 {
+    /*
     public float rotationSpeed = 90;
     public Transform target;
     public Vector3 offset = new Vector3(0,30,20);
@@ -24,5 +25,25 @@ public class ThirdPersonCameraController : MonoBehaviour
         {
             transform.LookAt(target.position);
         }
+    }
+    */
+
+    public float turnSpeed = 2.0f;
+    public Transform Ball;
+
+    private Vector3 offset;
+    private Vector3 baseOffset = new Vector3(0, 30, 20);
+
+    void Start()
+    {
+        offset = Ball.position + baseOffset;
+    }
+
+    void LateUpdate()
+    {
+        float input = Input.GetAxisRaw("PanRight") - Input.GetAxisRaw("PanLeft");
+        offset = Quaternion.AngleAxis(-input * turnSpeed, Vector3.up) * offset;
+        transform.position = Ball.position + offset;
+        transform.LookAt(Ball.position);
     }
 }
