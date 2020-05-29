a2d28e316fefb89502dfc77e022455c41ed5e62d
Alistair <AlistairJR302@Gmail.com>
2020-05-29 13:02:19 +0100

    More Shop Models

---
+++ HighPixelGameJam/Assets/Scripts/ChangeObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeObject : MonoBehaviour
+{
+    public GameObject normalAsset;
+    public GameObject parallelAsset;
+
+    public void NormalToParallel()
+    {
+        Instantiate(parallelAsset, normalAsset.transform.position, normalAsset.transform.rotation);
+        Destroy(normalAsset);
+    }
+
+    public void ParallelToNormal()
+    {
+        Instantiate(normalAsset, parallelAsset.transform.position, parallelAsset.transform.rotation);
+        Destroy(parallelAsset);
+    }
+}
---
+++ HighPixelGameJam/Assets/Scripts/DisableObjects.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableObjects : MonoBehaviour
+{
+    [Tooltip("0 = appear in normal, 1 = appear in parallel")]
+    public int world;
+
+    public void NormalToParallel()
+    {
+        if (world == 0)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+
+    public void ParallelToNormal()
+    {
+        if (world == 0)
+        {
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
